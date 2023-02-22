58924e9a3aec5a2763affca8808c0ddf06ba0672
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-02-22 10:16:31 +0100

    feat: rotating box, gitignore update

--- PhysXFramework_x64_Start/TestScene.cpp
+++ PhysXFramework_x64_Start/TestScene.cpp
@@ -3,14 +3,20 @@
 
 #include "Logger.h"
 
+#include "CubePosColorNorm.h"
+
 void TestScene::Initialize()
 {
+	m_pBox = new CubePosColorNorm(5.f, 2.f, 1.f);
+	AddGameObject(m_pBox);
 
+	m_pBox->Translate(0,1.f,0);
 }
 
 void TestScene::Update()
 {
-
+	const float totalTime{ GetSceneContext().GetGameTime()->GetTotal() };
+	m_pBox->Rotate(0, totalTime, 0);
 }
 
 void TestScene::Draw() const
